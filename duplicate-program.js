d988fe43e116a3c8a83213180d1ae473539f94e9
bhaskar-46 <appasanibhaskar1@gmail.com>
2024-05-03 17:18:22 +0530

    exporting example

---
+++ duplicate-program.js
@@ -0,0 +1,20 @@
+
+function duplicate(arr) {
+    var arr1 = [];
+    let k = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] == arr[j]) {
+                arr1[k] = arr[i];
+                k++;
+            }
+        }
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        console.log(arr1[i]);
+    }
+
+}
+
+module.exports = duplicate;
---
+++ test.js
@@ -0,0 +1,4 @@
+
+var duplicateFun = require("./duplicate-program");
+var arr = [10, 20, 30, 10, 20, 30, 40];
+duplicateFun(arr);
