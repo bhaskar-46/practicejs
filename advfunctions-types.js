e12316453f419af21ef2b151aac43cc59a7dc138
bhaskar-46 <appasanibhaskar1@gmail.com>
2024-05-04 12:42:11 +0530

    updated

--- advfunctions-types.js
+++ advfunctions-types.js
@@ -1,8 +1,9 @@
+/*---------------  Anonymous fun && Group function  -----------------------------------------------*/
 // var result = (function(x,y){
 //     return x + y;
 // })(5,6);
 // console.log("result :", result);
-/*-------------------------------------------------------------*/
+/*-----------------------  call back  --------------------------------------*/
 // function print(x) {
 //     for (let i = 0; i < 10; i++) {
 //         console.log(i);
@@ -13,29 +14,38 @@
 //     console.log(y)
 //     y()
 // }
-
 // write(print);
 
-// function add(x,y){
-//     var z = x + y;
-//     return z;
-// }
-
-/*------------------------------------------------------------------ */
+/*-----------------------  arrow function ------------------------------------------- */
 // var a = 12;
-// var add = function (x, y) {
+// var add = (x, y)=> {
 //     var z = x + y;
 //     return z;
 // }
 // var result = add(2, 5);
 // console.log(result);
-/*-----------------------------------------------------*/
-function f1(x) {
-    console.log(x);
-    x(10, 34, 78);
-}
-var f2 = function (a, b, c) {
-    console.log("hii", a, b, c)
-    console.log("inside f2");
-};
-f1(f2);
+/*------------------call back -----------------------------------*/
+// function f1(x) {
+//     console.log(x);
+//     x(10, 34, 78);
+// }
+// var f2 = function (a, b, c) {
+//     console.log("hii", a, b, c)
+//     console.log("inside f2");
+// };
+// f1(f2);
+
+/*-------------closure function-----------------*/
+function outerFunction(x) {
+    console.log("outer function....");
+
+    function innerFunction(y) {
+      return x + y;
+    }
+    console.log("below inner function..")
+    return innerFunction;
+  }
+  
+  const addFive = outerFunction(5);
+  var result = addFive(10);
+  console.log(result); // 15
